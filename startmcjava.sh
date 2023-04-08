a7d4b83dbbdbf9e99f81f6163021adb6e9ed26d0
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-04-08 20:06:41 +0800

    javaver

---
+++ startmcjava.sh
@@ -0,0 +1,30 @@
+jdk_version(){
+echo '====================='
+echo '选择Java版本'
+echo '====================='
+echo "1 为 Java8"
+echo "2 为 Java11"
+echo "3 Java16"
+echo "4 Java17"
+echo "5 自定义版本（未开发）"
+echo "请选择"
+read -p "输入: " JAVA_SOFTWARE </dev/tty
+
+case $JAVA_SOFTWARE in
+1)
+/jdk8/bin/java -Xms128M -Dfile.encoding=UTF-8 -Dlog4j2.formatMsgNoLookups=true -XX:+UseG1GC -jar server.jar nogui
+;;
+2)
+/jdk11/bin/java -Xms128M -Dfile.encoding=UTF-8 -Dlog4j2.formatMsgNoLookups=true -XX:+UseG1GC -jar server.jar nogui
+;;
+3)
+/jdk16/bin/java -Xms128M -Dfile.encoding=UTF-8 -Dlog4j2.formatMsgNoLookups=true -XX:+UseG1GC -jar server.jar nogui
+;;
+4)
+/jdk17/bin/java -Xms128M -Dfile.encoding=UTF-8 -Dlog4j2.formatMsgNoLookups=true -XX:+UseG1GC -jar server.jar nogui
+;;
+5)
+  echo '你来的太早了'
+;;
+esac
+}
